abd2f4eba39c1b4f5310ac0714d53570944b3a27
dongyun <ehddbs452100@naver.com>
2024-07-05 17:21:08 +0900

    :sparkles: Test

---
+++ src/main/java/DY/HaeDollarGo_Spring/TestController.java
@@ -0,0 +1,13 @@
+package DY.HaeDollarGo_Spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test(){
+        return "good";
+    }
+}
