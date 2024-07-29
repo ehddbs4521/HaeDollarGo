bf24649c8a9e24ce98afdbfb4429c81216b27036
dongyun <ehddbs452100@naver.com>
2024-07-29 22:09:15 +1000

    test

--- src/main/java/DY/HaeDollarGo_Spring/api/auth/jwt/TokenAuthenticationFilter.java
+++ src/main/java/DY/HaeDollarGo_Spring/api/auth/jwt/TokenAuthenticationFilter.java
@@ -31,6 +31,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+        log.info("uri: {}", request.getRequestURI());
         if (isUnprotectedEndpoint(request)) {
             filterChain.doFilter(request, response);
             return;
@@ -65,6 +66,6 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     private boolean isUnprotectedEndpoint(HttpServletRequest request) {
         String uri = request.getRequestURI();
-        return uri.equals("/") || uri.equals("/api-docs") || uri.equals("/swagger-ui.html");
+        return uri.equals("/") || uri.equals("/api-docs") || uri.equals("/swagger-ui/index.html");
     }
 }
--- src/main/java/DY/HaeDollarGo_Spring/api/auth/jwt/TokenExceptionFilter.java
+++ src/main/java/DY/HaeDollarGo_Spring/api/auth/jwt/TokenExceptionFilter.java
@@ -6,16 +6,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Slf4j
 public class TokenExceptionFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+        log.info("uri: {}", request.getRequestURI());
         if (isUnprotectedEndpoint(request)) {
             filterChain.doFilter(request, response);
             return;
@@ -33,7 +35,7 @@ public class TokenExceptionFilter extends OncePerRequestFilter {
 
     private boolean isUnprotectedEndpoint(HttpServletRequest request) {
         String uri = request.getRequestURI();
-        return uri.equals("/") || uri.equals("/api-docs") || uri.equals("/swagger-ui.html");
+        return uri.equals("/") || uri.equals("/api-docs") || uri.equals("/swagger-ui/index.html");
 
     }
 }
