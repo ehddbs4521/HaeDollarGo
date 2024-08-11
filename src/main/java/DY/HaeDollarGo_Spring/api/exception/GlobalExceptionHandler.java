62a37d9aaf69e96cc6d48e52499e34d8181751f6
dongyun <ehddbs452100@naver.com>
2024-08-11 19:12:44 +1000

    test

--- src/main/java/DY/HaeDollarGo_Spring/api/exception/GlobalExceptionHandler.java
+++ src/main/java/DY/HaeDollarGo_Spring/api/exception/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package DY.HaeDollarGo_Spring.api.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<?> handleCustomException(CustomException e) {
+        log.info("abc");
         return toResponse(e.getHttpStatus(), e.getCode(), e.getMessage());
     }
 
